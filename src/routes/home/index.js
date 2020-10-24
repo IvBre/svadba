ec741849d479f9bb6e4cee99f32165e7b8017a6d
Ivana Petrovic <ivana.petrovic@trivago.com>
2020-10-24 23:08:16 +0200

    added form

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,14 +1,12 @@
 import { h } from 'preact';
 import style from './style.css';
+import ViewInvitationForm from '../../components/invitation/index';
 
 const Home = () => (
 	<div class={style.home}>
 		<h1>Home</h1>
 		<p>This is the Home component.</p>
-		<form>
-			<input type="text" name="code" placeholder="Enter your invitation code here..."/>
-			<input type="submit" name="go" valu="GO" />
-		</form>
+		<ViewInvitationForm />
 	</div>
 );
 
