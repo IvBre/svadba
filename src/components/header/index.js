d642efb0f873de3692f6673c18d747fd0b53dd98
kdrechsler <kay.drechsler@trivago.com>
2021-04-13 22:31:11 +0200

    toggle Translation. unfinished Header navigation toggle.

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,27 +1,35 @@
-import { h } from 'preact';
+import { h, render } from 'preact';
+import React from 'react';
 import { Link } from 'preact-router/match';
 import style from './style.css';
 import MainComponent from "../translation";
 import { TranslateContext } from '@denysvuika/preact-translate';
-import {useContext} from "preact/hooks";
+import { useContext } from 'preact/hooks';
 
-const Header = () => {
-	const {t} = useContext(TranslateContext);
+export default class Header extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { translation: {}, classToggle: "" };
+		let {t} = useContext(TranslateContext);
+		this.state.translation = t
+		console.log(t);
+	}
 
-	return (
-		<header class={style.header}>
-			<h1>{t("title")} - {t("subtitle")} </h1>
-			<nav>
-				<Link activeClassName={style.active} href="/">{t("about")}</Link>
-				<Link activeClassName={style.active} href="/location">{t("location")}</Link>
-				<Link activeClassName={style.active} href="/transportation">{t("transportation")}</Link>
-				<Link activeClassName={style.active} href="/schedule">{t("schedule")}</Link>
-				<Link activeClassName={style.active} href="/tradition">{t("tradition")}</Link>
-				<Link activeClassName={style.active} href="/interesting">{t("interesting")}</Link>
-			</nav>
-			<MainComponent />
-		</header>
-	);
+	render() {
+		return (
+			<header class={style.header}>
+				<h1>{this.state.translation.t("title")} - {this.state.translation.t("subtitle")} </h1>
+				<nav>
+					<button type="button" class={ style.toggleButton }>Open navigation</button>
+					<Link activeClassName={style.active} href="/">{this.state.translation.t("about")}</Link>
+					<Link activeClassName={style.active} href="/location">{this.state.translation.t("location")}</Link>
+					<Link activeClassName={style.active} href="/transportation">{this.state.translation.t("transportation")}</Link>
+					<Link activeClassName={style.active} href="/schedule">{this.state.translation.t("schedule")}</Link>
+					<Link activeClassName={style.active} href="/tradition">{this.state.translation.t("tradition")}</Link>
+					<Link activeClassName={style.active} href="/interesting">{this.state.translation.t("interesting")}</Link>
+				</nav>
+				<MainComponent />
+			</header>
+		);
+	}
 };
-
-export default Header;
--- src/components/translation/index.js
+++ src/components/translation/index.js
@@ -1,33 +1,44 @@
 import { useContext } from 'preact/hooks';
+import React from 'react';
 import { TranslateContext } from '@denysvuika/preact-translate';
 import style from './style.css';
 import Cookies from 'js-cookie'
 
 export const AllowedLanguages = ["en", "de", "sr"];
 export const LanguageHeaderName = "language";
+export default class TranslationComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { translation: {}, classToggle: "" };
+        this.state.translation = useContext(TranslateContext);
+    }
 
-export default function TranslationComponent() {
-    const { setLang, t, lang } = useContext(TranslateContext);
 
-    function setCookieLang(lang) {
+    setCookieLang(lang) {
         if (!AllowedLanguages.includes(lang)) return;
 
         Cookies.set(LanguageHeaderName, lang);
-        setLang(lang)
+        this.state.translation.setLang(lang);
     }
 
-    let listClasses = classNames({
-        'translationList': true,
-    });
+    openTransList() {
+        if (this.state.classToggle == "") {
+            this.setState({ classToggle: `${ style.translationListOpen }` });
+        } else {
+            this.setState({ classToggle: "" });
+        }
+    }
 
-    return (
-        <section class={style.translation}>
-            <h3>Language: {lang}</h3>
-            <ul class={listClasses}>
-                <li><button onClick={() => setCookieLang('en')}>EN</button></li>
-                <li><button onClick={() => setCookieLang('sr')}>SR</button></li>
-                <li><button onClick={() => setCookieLang('de')}>DE</button></li>                
-            </ul>
-        </section>
-    );
+    render() {
+        return (
+            <section class={ style.translation }>
+                <button type="button" class="js-toggleTransList" onClick={ () => this.openTransList() }>Language: {this.state.translation.lang}</button>
+                <ul class={ `${ style.translationList } js-translationList ${ this.state.classToggle }`}>
+                    <li><button onClick={() => this.setCookieLang('en')}>English</button></li>
+                    <li><button onClick={() => this.setCookieLang('sr')}>Српски</button></li>
+                    <li><button onClick={() => this.setCookieLang('de')}>Deutsch</button></li>                
+                </ul>
+            </section>
+        );
+    }
 }
